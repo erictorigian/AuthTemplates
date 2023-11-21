394fc3862dd12180cd3babc455b03b598f6a1fea
Eric Torigian <eric@torigian.com>
2023-11-21 15:07:42 -0500

    Initial Commit

---
+++ AuthTemplates/AuthTemplatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthTemplatesApp.swift
+//  AuthTemplates
+//
+//  Created by Eric Torigian on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct AuthTemplatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
